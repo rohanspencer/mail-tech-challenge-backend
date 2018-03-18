8174d6c2f94f7ce90e196a0a05675cf0182305c9
rohanspencer <robo.spencer@gmail.com>
2018-03-18 21:49:14 +1100

    made to send via sendgrid then failover, mail not working with mailgun

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+var dotenv = require('dotenv')
+
+const result = dotenv.config()
+
+if (result.error) {
+    throw result.error
+}
+
+console.log(result.parsed)
+
 var express = require('express')
 var cors = require('cors')
 
 const PORT = 3001
 
 const app = express()
-const request = require('request')
+const fetch = require('node-fetch')
 
 app.use(cors())
 app.use(express.json())
@@ -22,52 +32,128 @@ app.post('/send', function(req, res) {
         var recipients = req.body.recipient_emails
         var subject = req.body.subject
         var body = req.body.body
+        console.log('recipeints', recipients)
 
         //construct json for sendgrid call
-        var sendgrid = {
-            personalizations: [
-                {
-                    to: [],
-                    subject: subject,
-                },
-            ],
-            from: {
-                email: sender,
-            },
-            content: [
-                {
-                    type: 'text/plain',
-                    value: body,
-                },
-            ],
-        }
-
+        var invalidRecipeints = false
         for (var i in recipients) {
-            sendgrid.personalizations.to.push({ email: i.recipient })
+            //allow for name but ensure email is there
+            if (
+                !(
+                    recipients[i].hasOwnProperty('email') &&
+                    Object.keys(recipients[i]).length <= 2
+                )
+            ) {
+                invalidRecipeints = true
+            }
         }
-
-        var options = {
-            url: 'https://api.sendgrid.com/v3/mail/send',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: 'Bearer ' + process.env.SENDGRID_API_KEY,
-                method: 'POST',
-            },
+        if (!invalidRecipeints) {
+            sendSendgrid(sender, recipients, subject, body)
+            var status = 200
+            res
+                .status(status)
+                .set('Content-Type', 'text/html')
+                .send('{}')
         }
-
-        request(options, function(error, response, body) {
-            console.log('error', error)
-            console.log('resp', response)
-            console.log('body', body)
-        })
-
-        //send http request to
-        res
-            .status(200)
-            .set('Content-Type', 'application/json')
-            .send('{"string":"hello world"}')
     }
 })
+
+function sendSendgrid(sender, recipients, subject, body) {
+    var sendgridBody = {
+        personalizations: [
+            {
+                to: recipients,
+                subject: subject,
+            },
+        ],
+        from: {
+            email: sender,
+        },
+        reply_to: {
+            email: sender,
+        },
+        subject: subject,
+        content: [
+            {
+                type: 'text/html',
+                value: '<html><p>' + body + '</p></html>',
+            },
+        ],
+    }
+
+    console.log('sending', sendgridBody)
+
+    var sendgrid = {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json, text/plain, */*',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-AU,en-GB;q=0.9,en-US;q=0.8,en;q=0.7',
+            Authorization: 'Bearer ' + process.env.SENDGRID_API_KEY,
+            Connection: 'keep-alive',
+            'Content-Length': '358',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(sendgridBody),
+    }
+
+    console.log(process.env.SENDGRID_API_KEY)
+    // send mail via sendgrid
+    fetch('https://api.sendgrid.com/v3/mail/send', sendgrid)
+        .then(sendgridRes => {
+            if (sendgridRes.status != 202) {
+                sendMailgun(sender, recipients, subject, body)
+            }
+        })
+        .catch(sendgirdError => {
+            sendMailgun(sender, recipients, subject, body)
+        })
+}
+
+function sendMailgun(sender, recipients, subject, body) {
+    var mailgunTo = ''
+
+    for (var i in recipients) {
+        if (mailgunTo != '') {
+            mailgunTo = mailgunTo + ','
+        }
+        mailgunTo = mailgunTo + recipients[i].email
+    }
+
+    var mailgunBody = {
+        from: sender,
+        to: mailgunTo,
+        subject: subject,
+        text: body,
+        html: body,
+    }
+
+    console.log('sending', mailgunBody)
+
+    var mailgun = {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Accept-Encoding': 'gzip, deflate, br',
+            Connection: 'keep-alive',
+            'Content-Length': '358',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(mailgunBody),
+    }
+
+    fetch(
+        'https://api:' +
+            process.env.MAILGUN_API_KEY +
+            '@api.mailgun.net/v3/messages',
+        mailgun
+    )
+        .then(mailgunRes => {
+            console.log(mailgunRes.status)
+            console.log('mailgun ', mailgunRes)
+        })
+        .catch(mailgunError => {})
+}
 
 app.listen(PORT, err => {
     if (err) {
